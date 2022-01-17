4ae018d5af46b1862313dd2b9b3268352a10e7dd
leenaAhmed <leena3792@gmail.com>
2022-01-17 21:25:05 +0200

    add main file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Movies from "./components/Movies/movieList/index";
+import Navbar from "./components/NavBar/nav";
+import MoviesDetails from "./components/Movies/movieDetails/index";
+import Footer from "./components/footer/footer";
+
+function App() {
+  return (
+    <>
+     
+    </>
+  );
+}
+
+export default App;
