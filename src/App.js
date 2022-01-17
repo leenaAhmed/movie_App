c9947c31016967d8d8af04255ac6a8f02bbf77ee
martinasamoule <61949296+martinasamoule@users.noreply.github.com>
2022-01-17 22:13:45 +0200

    add navbar to app file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Movies from "./components/Movies/movieList/index";
-import Navbar from "./components/NavBar/nav";
+import Navbar from "../navbar/navbar";
 import MoviesDetails from "./components/Movies/movieDetails/index";
 import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
-     
+     <Navbar/>
     </>
   );
 }
